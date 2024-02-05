372595db726592b9f06e4746efb22628696de991
danielecarpentiero <carpentierodaniele@gmail.com>
2024-02-05 19:10:35 +0100

    feat: create & edit views

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,44 +2,53 @@
 
 @section('main')
     <div class="container">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
         <h2 class="text-center">Insert new comic</h2>
         <form action="{{route('comics.store')}}" method="post">
             @csrf
             <div class="mb-3">
                 <label class="form-label">title</label>
-                <input type="text" class="form-control" name="title">
+                <input type="text" class="form-control" name="title" value="{{old('title')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">description</label>
-                <textarea class="form-control"  rows="3" name="description"></textarea>
+                <textarea class="form-control"  rows="3" name="description" value="{{old('description')}}"></textarea>
             </div>
             <div class="mb-3">
                 <label class="form-label">Link your image!</label>
-                <input type="text" class="form-control" name="thumb">
+                <input type="text" class="form-control" name="thumb" value="{{old('thumb')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">price</label>
-                <input type="text" class="form-control" name="price">
+                <input type="text" class="form-control" name="price" value="{{old('price')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">series</label>
-                <input type="text" class="form-control" name="series">
+                <input type="text" class="form-control" name="series" value="{{old('series')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">Sale date</label>
-                <input type="text" class="form-control" name="sale_date">
+                <input type="text" class="form-control" name="sale_date" value="{{old('sale_date')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">type</label>
-                <input type="text" class="form-control" name="type">
+                <input type="text" class="form-control" name="type" value="{{old('type')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">artists</label>
-                <input type="text" class="form-control" name="artists">
+                <input type="text" class="form-control" name="artists" value="{{old('artists')}}">
             </div>
             <div class="mb-3">
                 <label class="form-label">writers</label>
-                <input type="text" class="form-control" name="writers">
+                <input type="text" class="form-control" name="writers" value="{{old('writers')}}">
             </div>
 
             <button type="submit" class="btn btn-primary">Submit</button>
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -8,39 +8,39 @@
         @method('patch')
         <div class="mb-3">
             <label class="form-label">title</label>
-            <input type="text" class="form-control" name="title" value="{{$comic->title}}">
+            <input type="text" class="form-control" name="title" value="{{old('title'), $comic->title}}">
         </div>
         <div class="mb-3">
             <label class="form-label">description</label>
-            <textarea class="form-control"  rows="3" name="description" value="{{$comic->description}}" ></textarea>
+            <textarea class="form-control"  rows="3" name="description" value="{{old('description'), $comic->description}}" ></textarea>
         </div>
         <div class="mb-3">
             <label class="form-label">Link your image!</label>
-            <input type="text" class="form-control" name="thumb" value="{{$comic->thumb}}">
+            <input type="text" class="form-control" name="thumb" value="{{old('thumb'), $comic->thumb}}">
         </div>
         <div class="mb-3">
             <label class="form-label">price</label>
-            <input type="text" class="form-control" name="price" value="{{$comic->price}}">
+            <input type="text" class="form-control" name="price" value="{{old('price'), $comic->price}}">
         </div>
         <div class="mb-3">
             <label class="form-label">series</label>
-            <input type="text" class="form-control" name="series" value="{{$comic->series}}">
+            <input type="text" class="form-control" name="series" value="{{old('series'), $comic->series}}">
         </div>
         <div class="mb-3">
             <label class="form-label">Sale date</label>
-            <input type="text" class="form-control" name="sale_date" value="{{$comic->sale_date}}">
+            <input type="text" class="form-control" name="sale_date" value="{{old('sale_date'), $comic->sale_date}}">
         </div>
         <div class="mb-3">
             <label class="form-label">type</label>
-            <input type="text" class="form-control" name="type" value="{{$comic->type}}">
+            <input type="text" class="form-control" name="type" value="{{old('type'), $comic->type}}">
         </div>
         <div class="mb-3">
             <label class="form-label">artists</label>
-            <input type="text" class="form-control" name="artists" value="{{implode(',', $comic->artists)}}">
+            <input type="text" class="form-control" name="artists" value="{{old('artists'), implode(',', $comic->artists)}}">
         </div>
         <div class="mb-3">
             <label class="form-label">writers</label>
-            <input type="text" class="form-control" name="writers" value="{{implode(',', $comic->writers)}}">
+            <input type="text" class="form-control" name="writers" value="{{old('writers'), implode(',', $comic->writers)}}">
         </div>
 
         <button type="submit" class="btn btn-primary">Submit</button>
